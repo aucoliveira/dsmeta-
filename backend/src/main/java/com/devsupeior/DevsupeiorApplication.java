e351f07dd7c937246cb1174c1e685ae67bcf492d
Augusto Cesar <azotaugusto@gmail.com>
2022-07-14 12:01:17 -0300

    Subindo para o heroku

---
+++ backend/src/main/java/com/devsupeior/DevsupeiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsupeior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsupeiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsupeiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/devsupeior/controller/SaleController.java
@@ -0,0 +1,37 @@
+package com.devsupeior.controller;
+
+import com.devsupeior.entities.Sale;
+import com.devsupeior.service.SaleService;
+import com.devsupeior.service.SmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService saleService;
+
+    @Autowired
+    private SmsService smsService;
+
+    @GetMapping
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return saleService.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notication")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
+    }
+
+
+}
